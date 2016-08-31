4987b594940416cec6815e333a761b534dea7ccf
Chris Eldridge <cre@mojotech.com>
2016-08-31 14:31:16 -0400

    use react-dom to bind react component to dom id

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,22 @@
-console.log('webpack jsx goat')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const domNode = document.getElementById('react');
+
+const Goat = () => (
+  <div>
+    <button onClick={() => console.log('goat')}>clickable goat</button>
+    <a
+      href="https://s3-us-west-2.amazonaws.com/goatz/goat.jpg"
+      target="_blank"
+    >
+      see a goat
+    </a>
+    <img
+      src="https://s3-us-west-2.amazonaws.com/goatz/goat.jpg"
+      alt="alt"
+    />
+  </div>
+);
+
+ReactDOM.render(<Goat />, domNode);
